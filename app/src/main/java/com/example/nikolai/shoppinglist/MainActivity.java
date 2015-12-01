09d60b7e9e8bc06c3281e6a42ea8c2419c2f6774
Nikolai94 <nikolai@cybermanager.dk>
2015-12-01 18:51:57 +0100

    Database

--- app/src/main/java/com/example/nikolai/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/nikolai/shoppinglist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nikolai.shoppinglist;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.View;
@@ -12,9 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.example.nikolai.shoppinglist.dataSourceLayer.ShoppingListDb;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,26 +27,35 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     List<String> shoppingLists;
     ListView list;
+    Cursor cursor;
+    Adapter adapter;
+    private ShoppingListDb db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new ShoppingListDb(this);
+        db.open();
+
+
+
+
+        db.createShoppingList("Nytår ", "31-12-2015", "0");
+        db.createShoppingList("jul ", "24-12-2015", "0");
+        db.createShoppingList("Sannes Fødselsdag ", "01-02-2016","0");
+
         shoppingLists = new ArrayList<>();
+
+        cursor = db.getShoppingLists();
+        startManagingCursor(cursor);
+        adapter = new SimpleCursorAdapter(this, R.layout.content_main, cursor, FROM, 0, 0);
+        list.setAdapter(adapter);
+
+
         list = (ListView)findViewById(R.id.listView);
-        shoppingLists.add("Nytår");
-        shoppingLists.add("Jul");
-        shoppingLists.add("1 December");
-        shoppingLists.add("Nikolines fødsesldag");
-        shoppingLists.add("her fødsesldag");
-        shoppingLists.add("her fødsesldag");
-        shoppingLists.add("her ");
-        shoppingLists.add("her hdfhdfh");
-        shoppingLists.add("dfhfdhdh fødsesldag");
-        shoppingLists.add("herh hdf");
-        shoppingLists.add("hdfhher føhdfdsesldag");
-        shoppingLists.add("hehdfhr føhdfhdsesldag");
+
 
         updateList();
 
@@ -50,17 +64,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setSupportActionBar(toolbar);
 
 
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-*/
-
-        /*ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-        */
 
     }
 
@@ -102,85 +106,4 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
     }
 
-    /*@Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }*/
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }*/
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.new_game:
-                //newGame();
-                return true;
-            case R.id.help:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }*/
-
-    /* @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-*/
-/*
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-        */
-
-    /*    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }*/
-
-
-    //Metode til at starte ny Activity, når der er klikket på en indkøbsliste.
-
-    /*
-        Toast.makeText(this, "view clicked at #" + position, Toast.LENGTH_LONG).show();
-        shoppingLists.remove(position);
-        updateList();
-        */
 }
